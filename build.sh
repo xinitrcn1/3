24258173dcad796bb5fedce1c240cbb1e7a23fb2
lizzie <lizzie@eden-emu.dev>
2026-06-25 23:30:02 +0000

    evil newlib

--- build.sh
+++ build.sh
@@ -4,6 +4,9 @@ NPROC=8
 PATCHDIR="$PWD/patches"
 CACHEDIR="$PWD/prefix/cache"
 PREFIX="$PWD/prefix"
+
+export PS3DEV="$PREFIX"
+export PSL1GHT="$PS3DEV"
 export PATH="$PREFIX/bin:$PATH"
 
 make_prefix() {
@@ -83,37 +86,37 @@ build_gcc() {
     git_initialize "$CACHEDIR/gcc-$VERSION"
     git -C "$CACHEDIR/gcc-$VERSION" diff --cached >"$PATCHDIR/gcc-$VERSION.patch"
 
-    # TARGET=powerpc64-ps3-elf
-    # mkdir -p "$CACHEDIR/build-gcc-$TARGET"
-    # cd "$CACHEDIR/build-gcc-$TARGET"
-    # [ -f "$CACHEDIR/build-gcc-$TARGET/Makefile" ] \
-    #     || "$CACHEDIR/gcc-$VERSION/configure" \
-    #     --prefix="$PREFIX" \
-    #     --target="$TARGET" \
-    #     --disable-dependency-tracking \
-    #     --disable-libcc1 \
-    #     --disable-libstdcxx-pch \
-    #     --disable-multilib \
-    #     --disable-nls \
-    #     --disable-shared \
-    #     --disable-win32-registry \
-    #     --disable-bootstrap \
-    #     --enable-languages="c" \
-    #     --enable-long-double-128 \
-    #     --enable-lto \
-    #     --enable-threads \
-    #     --with-cpu="cell" \
-    #     --with-newlib \
-    #     --enable-newlib-multithread \
-    #     --enable-newlib-hw-fp \
-    #     --with-system-zlib
-    # cd "$CACHEDIR/build-gcc-$TARGET" \
-    #     && gmake -j$NPROC all-gcc \
-    #     && gmake -j$NPROC all-target-libgcc \
-    #     && gmake -j$NPROC all-target-libstdc++-v3 \
-    #     && gmake install-gcc \
-    #     && gmake install-target-libgcc \
-    #     && gmake install-target-libstdc++-v3
+    TARGET=powerpc64-ps3-elf
+    mkdir -p "$CACHEDIR/build-gcc-$TARGET"
+    cd "$CACHEDIR/build-gcc-$TARGET"
+    [ -f "$CACHEDIR/build-gcc-$TARGET/Makefile" ] \
+        || "$CACHEDIR/gcc-$VERSION/configure" \
+        --prefix="$PREFIX" \
+        --target="$TARGET" \
+        --disable-dependency-tracking \
+        --disable-libcc1 \
+        --disable-libstdcxx-pch \
+        --disable-multilib \
+        --disable-nls \
+        --disable-shared \
+        --disable-win32-registry \
+        --disable-bootstrap \
+        --enable-languages="c" \
+        --enable-long-double-128 \
+        --enable-lto \
+        --enable-threads \
+        --with-cpu="cell" \
+        --with-newlib \
+        --enable-newlib-multithread \
+        --enable-newlib-hw-fp \
+        --with-system-zlib
+    cd "$CACHEDIR/build-gcc-$TARGET" \
+        && gmake -j$NPROC all-gcc \
+        && gmake -j$NPROC all-target-libgcc \
+        && gmake -j$NPROC all-target-libstdc++-v3 \
+        && gmake install-gcc \
+        && gmake install-target-libgcc \
+        && gmake install-target-libstdc++-v3
 
     TARGET=spu-unknown-elf
     mkdir -p "$CACHEDIR/build-gcc-$TARGET"
@@ -137,15 +140,41 @@ build_gcc() {
         --enable-newlib-multithread \
         --enable-newlib-hw-fp \
         --with-pic
+    # cd "$CACHEDIR/build-gcc-$TARGET" \
+    #     && gmake -j$NPROC all-gcc \
+    #     && gmake -j$NPROC all-target-libgcc \
+    #     && gmake -j$NPROC all-target-libstdc++-v3 \
+    #     && gmake install-gcc \
+    #     && gmake install-target-libgcc \
+    #     && gmake install-target-libstdc++-v3
+    # TODO: ICE when scheduling with libgcc
     cd "$CACHEDIR/build-gcc-$TARGET" \
         && gmake -j$NPROC all-gcc \
-        && gmake -j$NPROC all-target-libgcc \
-        && gmake -j$NPROC all-target-libstdc++-v3 \
-        && gmake install-gcc \
-        && gmake install-target-libgcc \
-        && gmake install-target-libstdc++-v3
+        && gmake install-gcc
+
+    touch "$PREFIX/build-gcc"
+}
+
+build_newlib() {
+    VERSION=1.20.0
+    download_cached "newlib-$VERSION.tar.gz" "https://sourceware.org/pub/newlib/newlib-$VERSION.tar.gz"
+    [ -d "$CACHEDIR/newlib-$VERSION" ] || tar -xvzf "$CACHEDIR/newlib-$VERSION.tar.gz" -C "$CACHEDIR"
+    git_initialize "$CACHEDIR/newlib-$VERSION"
+    git -C "$CACHEDIR/newlib-$VERSION" diff --cached >"$PATCHDIR/newlib-$VERSION.patch"
+
+}
+
+build_psl1ght() {
+    COMMIT=f987683
+    download_cached "psl1ght.tar.xz" "https://github.com/ps3dev/psl1ght/tarball/master"
+    [ -d "$CACHEDIR/ps3dev-PSL1GHT-$COMMIT" ] || tar -xvzf "$CACHEDIR/psl1ght.tar.xz" -C "$CACHEDIR"
+    cd "$CACHEDIR/ps3dev-PSL1GHT-$COMMIT" \
+        && gmake -j$NPROC \
+        && gmake install
 }
 
 make_prefix
 [ -f "$PREFIX/build-binutils" ] || build_binutils
 [ -f "$PREFIX/build-gcc" ] || build_gcc
+build_newlib
+#build_psl1ght
